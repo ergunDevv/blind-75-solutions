71daa8f462b836738650acdf30eb6345fd7b6ac3
ergunDevv <ergncyln2127088@gmail.com>
2023-05-03 11:11:11 +0300

    BestTimeToBuyAndSellStock question and soluton added

--- Array/1-)TwoSum.py
+++ Array/1-)TwoSum.py
@@ -1,19 +1,20 @@
 # QUESTION :
 # Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.You may assume that each input would have exactly one solution, and you may not use the same element twice.You can return the answer in any order.
 
-def twoSum(arr,target) :
-    prevHashMap={} # storing data as value: index pairs
+def twoSum(arr, target):
+    prevHashMap = {}  # storing data as value: index pairs
 
     for i, n in enumerate(arr):
-        diff=target-n
-        if diff in prevHashMap: #Searching difference between target and number if difference exists in the prevHashMap. Returning the indexes of values.
-        # If not exists in the prevHashMap loop continues
-            return[prevHashMap[diff],i]
+        diff = target-n
+        # Searching difference between target and number if difference exists in the prevHashMap. Returning the indexes of values.
+        if diff in prevHashMap:
+            # If not exists in the prevHashMap loop continues
+            return [prevHashMap[diff], i]
 
         print('i'+str(i))
         # print('prevHashMap'+str(prevHashMap[n]))
-        prevHashMap[n]=i
+        prevHashMap[n] = i
     return
 
 
-print(twoSum([2,7,11,15],9)) 
+print(twoSum([2, 7, 11, 15], 9))
--- Array/2-)BestTimeToBuyAndSellStock.py
+++ Array/2-)BestTimeToBuyAndSellStock.py
@@ -1,3 +1,6 @@
+# QUESTION : 
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
 def maxProfitOfStock(prices):
     # Using the sliding window method(Two pointers)
     l=0 #The first pointer will store the index of the lowest price
