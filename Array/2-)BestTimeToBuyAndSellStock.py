3055a8728f5ae02a4f281d663b9103a7603dcdc7
ergunDevv <ergncyln2127088@gmail.com>
2023-05-04 11:11:11 +0300

    ContainsDuplicate question and soluton added

--- Array/2-)BestTimeToBuyAndSellStock.py
+++ Array/2-)BestTimeToBuyAndSellStock.py
@@ -1,25 +1,27 @@
-# QUESTION : 
+# QUESTION :
 # You are given an array prices where prices[i] is the price of a given stock on the ith day.You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
 def maxProfitOfStock(prices):
     # Using the sliding window method(Two pointers)
-    l=0 #The first pointer will store the index of the lowest price
-    r=1 #The second pointer will store the index of the highest price
-    maxProfit=0  #The maxProfit will store the difference between highest price and lowest price
+    l = 0  # The first pointer will store the index of the lowest price
+    r = 1  # The second pointer will store the index of the highest price
+    maxProfit = 0  # The maxProfit will store the difference between highest price and lowest price
 
-    while r<len(prices): #The loop continues until the last element of the prices array.
-        if(prices[l]<prices[r]): # If the index of the right pointer value in the prices array is less than the index of the left pointer value. Check if profit is higher than maxProfit
-            profit=prices[r]-prices[l]
+    # The loop continues until the last element of the prices array.
+    while r < len(prices):
+        if (prices[l] < prices[r]):  # If the index of the right pointer value in the prices array is less than the index of the left pointer value. Check if profit is higher than maxProfit
+            profit = prices[r]-prices[l]
             # If profit is higher than maxProfit. Change profit to maxProfit
-            maxProfit=max(profit,maxProfit)
+            maxProfit = max(profit, maxProfit)
         else:
             # If the index of the right pointer value in the prices array is less than the index of the left pointer value. Replace the index of the left pointer with the index of the right pointer.
-            l=r
-        #Increase r to 1 for each cycle
-        r+=1
+            l = r
+        # Increase r to 1 for each cycle
+        r += 1
     return maxProfit
 
-print('Output:',maxProfitOfStock([7,1,5,3,6,4]))
+
+print('Output:', maxProfitOfStock([7, 1, 5, 3, 6, 4]))
 # Output: 5
-print('Output:',maxProfitOfStock([7,6,4,3,1]))
+print('Output:', maxProfitOfStock([7, 6, 4, 3, 1]))
 # Output: 0
--- Array/3-)ContainsDuplicate.py
+++ Array/3-)ContainsDuplicate.py
@@ -1,12 +1,17 @@
+# QUESTION : 
+# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 def containsDuplicate(arr):
+    # Creating Hashmap.
     duplicateHashMap = {}
-
+    # Looping every element of array
     for i, n in enumerate(arr):
+        # If element exists in duplicateHashMap return true if not exists append the element to duplicateHashMap
         if n in duplicateHashMap:
             return True
         else:
             duplicateHashMap[n] = i
+    # If didn't return true after loop return false
     return False
 
 
