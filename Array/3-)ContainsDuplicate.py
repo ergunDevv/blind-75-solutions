ead0587f44ef53a4f2a35ccb59db26ca784f2e9d
ergunDevv <ergncyln2127088@gmail.com>
2023-05-05 11:11:11 +0300

    Reverse Linked List question

--- Array/3-)ContainsDuplicate.py
+++ Array/3-)ContainsDuplicate.py
@@ -1,4 +1,4 @@
-# QUESTION : 
+# QUESTION :
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 def containsDuplicate(arr):
